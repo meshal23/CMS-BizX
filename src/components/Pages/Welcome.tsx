f0653dda6f06fbf48f174f907845f3fdc8c0e4b8
meshalTitum97 <meshal@titumlk.onmicrosoft.com>
2024-11-07 11:45:52 +0530

    Update Welcome.tsx
    added animation for welcome screen

--- src/components/Pages/Welcome.tsx
+++ src/components/Pages/Welcome.tsx
@@ -6,7 +6,7 @@ function Welcome() {
       <div className="relative z-10 max-w-screen-xl px-4 py-8 mx-auto text-center lg:py-16">
         <a
           href="#"
-          className="inline-flex items-center justify-between px-1 py-1 text-sm text-blue-700 bg-blue-100 rounded-full pe-4 mb-7 dark:bg-blue-900 dark:text-blue-300 hover:bg-blue-200 dark:hover:bg-blue-800"
+          className="inline-flex items-center justify-between px-1 py-1 text-sm text-blue-700 bg-blue-100 rounded-full motion-preset-slide-right motion-ease-spring-bounciest motion-duration-1500 pe-4 mb-7 dark:bg-blue-900 dark:text-blue-300 hover:bg-blue-200 dark:hover:bg-blue-800"
         >
           <span className="text-xs bg-blue-600 rounded-full text-white px-4 py-1.5 me-3">
             New
@@ -31,10 +31,10 @@ function Welcome() {
             />
           </svg>
         </a>
-        <h1 className="mb-4 text-4xl font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-6xl dark:text-white">
+        <h1 className="mb-4 text-4xl font-extrabold leading-none tracking-tight text-gray-900 motion-preset-slide-left motion-duration-1500 motion-ease-spring-bouncy md:text-5xl lg:text-6xl dark:text-white">
           We invest in the world’s potential
         </h1>
-        <p className="mb-8 text-lg font-normal text-gray-500 lg:text-xl sm:px-16 lg:px-48 dark:text-gray-200">
+        <p className="mb-8 text-lg font-normal text-gray-500 motion-preset-fade motion-duration-1000 motion-ease-spring-smooth lg:text-xl sm:px-16 lg:px-48 dark:text-gray-200">
           Here at Flowbite we focus on markets where technology, innovation, and
           capital can unlock long-term value and drive economic growth.
         </p>
@@ -42,7 +42,7 @@ function Welcome() {
         <NavLink to="/login">
           <button
             type="submit"
-            className="text-white text-center w-64 p-5  end-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+            className="text-white text-center w-64 p-5 motion-preset-bounce motion-duration-1000 motion-ease-spring-bouncy end-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
           >
             Login
           </button>
